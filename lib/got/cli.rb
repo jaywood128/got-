d23df3ce278754a29346534587157c8dc4b94213
Johnathon Wood <jaywood128@gmail.com>
2018-12-17 17:55:34 +0000

    Updated list_books

--- lib/got/cli.rb
+++ lib/got/cli.rb
@@ -7,13 +7,34 @@ class Got::CLI
     DOC
     list_books
     menu
+    list_characters
   end
 
   def menu
-    puts "Enter the book you want more info on:"
+    puts "Enter the book you want more info on. For exiting, type 'exit' and to see the books again 'list_books':"
+    input = nil
+    while input != 'exit'
 
+      input = gets.strip
+      case input
+      when 'book1'
+          puts "more info on book1"
+      when 'book2'
+          puts "More book2"
+      when 'book3'
+          puts "more info book3"
+      when 'book4'
+          puts "more info book4"
+      when 'book5'
+        puts "more info book5"
+      when 'list_books'
+        list_books
+    else
+      puts "Invalid input"
+    end
 
   end
+end
 
   def list_books
     puts "book1"
