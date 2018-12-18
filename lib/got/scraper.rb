209c154734a158bf5ab62989cf86bc9e1d45ab84
Johnathon Wood <jaywood128@gmail.com>
2018-12-18 18:54:16 +0000

    Httparty uri get request

--- lib/got/scraper.rb
+++ lib/got/scraper.rb
@@ -1,18 +1,24 @@
 require 'pry'
-require_relative 'httparty'
+require 'httparty'
 class Got::Scraper
+  extend Got
 
+  include HTTParty
   format :json
 
   BASE_ENDPOINT  = "http://anapioficeandfire.com/api".freeze
 
   WORD_PARTICLES = %w(and or the over to the a but of for with).freeze
 
-  def scrap
+  def self.get_books
+    # @get_books ||= begin
+    #   response    = get("#{BASE_ENDPOINT}/books?page=1&pageSize=50")
+    #   binding.pry
+    #   total_pages = get_page_count(response)
+    #
+    #   get_all_records('books', response.parsed_response, total_pages)
     binding.pry
-    BASE_ENDPOINT
+    response = self.class.get('{#BASE_ENDPOINT}/api/books')
 
-  end
-
-
+    end
 end
