5bdc75c1073f1ea743610fa493bb6380396fb316
Johnathon Wood <jaywood128@gmail.com>
2018-12-21 16:30:23 +0000

    Making progess

--- lib/got/Book.rb
+++ lib/got/Book.rb
@@ -14,6 +14,18 @@ class Got::Book
    @@all
  end
 
+ def get_more_info
+   book = get("#{BASE_ENDPOINT}/books")
+   book.map do |book|
+   <<-DOC isbn: #{book['isbn']}
+          authors: #{book['authors']}
+          puts number of pages: #{book['numberOfPages']}
+          puts publisher: #{book['publisher']}
+    DOC
+    binding.pry
+
+ end
+
  def search_by_character_name(name)
       self.all.each do |book|
       book[:character_ids].map do |url|
--- lib/got/api.rb
+++ lib/got/api.rb
@@ -9,17 +9,18 @@ class Got::API
 
   WORD_PARTICLES = %w(and or the over to the a but of for with).freeze
 
-  def self.get_books
-    response = get("#{BASE_ENDPOINT}/books?page=1&pageSize=50")
-    response.map do |book|
-      book_hash =  {:title => book['name'],
-        :author => book['authors'][0],
-      :number_of_pages => book['numberOfPages'],
-      :character_ids => book['characters'].map { |url| url.split("/").last.to_i}
-        }
+  def self.print_all_books
+    # response = get("#{BASE_ENDPOINT}/books?page=1&pageSize=50")
+    # response.map do |book|
+    #   book_hash =  {:title => book['name'],
+    #     :author => book['authors'][0],
+    #   :number_of_pages => book['numberOfPages'],
+    #   :character_ids => book['characters'].map { |url| url.split("/").last.to_i}
+    #     }
+    all_the_books = get("#{BASE_ENDPOINT}/books")
+    all_the_books.map do |book|
+      puts "Book #{book['url'].split.map {|url| url.split("/").last.to_i}} : #{book['name']}."
 
-      Got::Book.new(book_hash[:title], book_hash[:author], book_hash[:number_of_pages], book_hash[:character_ids])
-      binding.pry
   end
 
   end
