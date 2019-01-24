08ff057bb30712b33aa4cc6121290ae4f25c6e34
Johnathon Wood <jaywood128@gmail.com>
2019-01-24 23:11:23 +0000

    Fixed print_details_for_books

--- lib/got/cli.rb
+++ lib/got/cli.rb
@@ -34,7 +34,6 @@ class Got::CLI
   end
 
   def self.print_details_for_book(book)
-    character_count = []
     puts "Author: #{book.author}"
     puts "Number of pages: #{book.number_of_pages}"
     puts "Point of View Characters: "
@@ -43,9 +42,10 @@ class Got::CLI
     end
     puts " \n\n "
     puts "Total number of POV characters: #{book.characters.count}"
+
     char_number = gets.strip.to_i
     if (1..book.characters.count).include?(char_number)
-      book.characters[char_number - 1]
+      puts book.characters[char_number - 1].details
     end
      #based on the char_number, select the corresponding character.details
   end
@@ -71,37 +71,15 @@ class Got::CLI
         character_name = gets.strip
         character = Got::API.find_character_by_name(character_name)
           if character != false
-            puts "Character found! Type 'Y' for more info, otherwise type 'N'"
-            y_or_n = gets.strip
-            if y_or_n == 'Y'
-              puts character.details
-            elsif y_or_n == 'N'
-              puts "Enter another command: "
-            another_command = gets.strip
-              if another_command == "find character by name"
-                character_name = gets.strip
-                character = find_character_by_name(character_name)
-              elsif another_command == "print all books"
-                print_all_books
-            else
-              exit
-            end
+            puts character.details
+           # puts character.details #create an instance method in Character.rb that accesses the character details
+          else
+            puts "not found"
           end
-        end   # puts character.details #create an instance method in Character.rb that accesses the character details
-        else
-          puts "not found"
-          # character not found
-          # ask for input again
-          # input find character by name
-          # Got::API.find_character_by_name(character_name
-        end
-          # Return a character object or nil
           input = gets.strip
-        elsif input == "exit"
+      elsif input == "exit"
           exit
-        else
-          puts "Invalid response!"
-          input = gets.strip
+
         end
       end
     end
