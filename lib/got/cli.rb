192ddcf1c6eb39955b74b6c07995e2007e2914ab
Johnathon Wood <jaywood128@gmail.com>
2019-02-06 17:45:40 +0000

    Finished ..for now!

--- lib/got/cli.rb
+++ lib/got/cli.rb
@@ -17,68 +17,58 @@ class Got::CLI
       elsif input == "find character by name"
         character_finder
       elsif input == "menu"
-          menu
-        end
-      end
-    end
-    def self.menu
-      puts "These are your options, type them as is: "
-      puts "print all books"
-      puts "find character by name"
-      puts "exit"
-    end
-
-   def self.print_all_books
-     Got::Book.all.each.with_index(1) do |book, i| #value, index
-       puts "Name:#{i} #{book.title}"
-     end
-     puts "Enter book number for more info: "
-     book_number = gets.strip.to_i
-     if (1..12).include?(book_number)
-       book = Got::Book.all[book_number - 1]
-       print_details_for_book(book)
-     elsif book_number == 'exit'
-       exit
-     else
-       puts "Not found, try again: "
-       print_all_books
-     end
-   end
-   def self.print_details_for_book(book)
-     puts "Author: #{book.author}"
-     puts "Number of pages: #{book.number_of_pages}"
-     puts "Point of View Characters: "
-     book.characters.each.with_index do |char, i|
-       puts "#{i + 1}. #{char.name}"
-     end
-     puts " \n\n "
-     puts "Total number of POV characters: #{book.characters.count}"
-
-     char_number = gets.strip.to_i
-     if (1..book.characters.count).include?(char_number)
-       puts book.characters[char_number - 1].details
-     end
-   end
-
-    def self.character_finder
-      puts "Enter the name of the character you want more info on: "
-      character_name = gets.strip
-      character = Got::Character.find_by_name(character_name)
-        if character != false
-          puts character.details
-          # puts character.details #create an instance method in Character.rb that accesses the character details
-        else
-         puts "not found"
-        end
-    end
-
-  def self.print_books_characters
-    Got::Book.all do |book|
-      book.characters.each.with_index(1) do |character, i|
-        puts "#{i}. #{character}"
+        menu
       end
     end
   end
-
-
+  def self.menu
+    puts "These are your options, type them as is: "
+    puts "print all books"
+    puts "find character by name"
+    puts "exit"
   end
+
+ def self.print_all_books
+   Got::Book.all.each.with_index(1) do |book, i| #value, index
+     puts "Name:#{i} #{book.title}"
+   end
+   puts "Enter book number for more info: "
+   book_number = gets.strip.to_i
+   if (1..12).include?(book_number)
+     book = Got::Book.all[book_number - 1]
+     print_details_for_book(book)
+   elsif book_number == 'exit'
+     exit
+   else
+     puts "Not found, try again: "
+     print_all_books
+   end
+ end
+ def self.print_details_for_book(book)
+   puts "Author: #{book.author}"
+   puts "Number of pages: #{book.number_of_pages}"
+   puts "Point of View Characters: "
+   book.characters.each.with_index do |char, i|
+     puts "#{i + 1}. #{char.name}"
+   end
+   puts " \n\n "
+   puts "Total number of POV characters: #{book.characters.count}"
+
+   char_number = gets.strip.to_i
+   if (1..book.characters.count).include?(char_number)
+     puts book.characters[char_number - 1].details
+   end
+ end
+
+  def self.character_finder
+    puts "Enter the name of the character you want more info on: "
+    character_name = gets.strip
+    character = Got::Character.find_by_name(character_name)
+    if character != false
+      puts character.details
+      # puts character.details #create an instance method in Character.rb that accesses the character details
+    else
+      puts "not found"
+    end
+  end
+end
